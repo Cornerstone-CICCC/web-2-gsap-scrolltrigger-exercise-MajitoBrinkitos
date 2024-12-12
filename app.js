e22300495e8529a71aa1524aa89d2dc3de4cf5d8
Majito Brinkitos <mcvef2013@gmail.com>
2024-12-11 18:58:46 -0800

    2024-12-11

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 gsap.registerPlugin(ScrollTrigger)
 
 // YOUR CODE HERE
+//.line-one 
+const lineOneTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: 'line-one',
+        start: 'top top',
+        end: '+=500',
+        pin: true,
+        yoyo: true,
+        markers: true,
+        toggleActions: 'restart pause resume none',
+    }
+});
+
+//selecting span elements
+const spans = document.querySelectorAll('.line-one span');
+
+lineOneTl.from(spans, {
+    y: '25vh',
+    ease: 'none',
+    duration: 1,
+    opacity: 0,
+    stagger: 0.2,
+});
